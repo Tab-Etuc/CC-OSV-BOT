c2126fb0f75c65af31a1e456df58d8c81281027a
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-07-10 19:18:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ PINK_COLOR = 0xe0b3c7
 PINK_COLOR_2 = 0xFFC0CB
 
 Prefix = "C","c"
-Owner_id = 806346991730819121
+Owner_id = "806346991730819121"
 好喔_pic = "https://cdn.discordapp.com/attachments/834074590720294912/854959330671591435/unknown.png"
 WEBHOOK_URL = "https://discord.com/api/webhooks/859633422498398288/5JVnexiCnP3BIk-kLPuAk4xDqadplNzgv-85zyS25poVDjhFjPwXz1CX0SDkbUlkcSwb"
 
