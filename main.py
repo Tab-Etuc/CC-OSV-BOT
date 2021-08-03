d5bb0ae2083249132bf6220f723a72dca007af0c
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2021-08-03 20:23:35 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,24 +3,40 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 from config import *
+from itertools import cycle
 
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-    
+
 intents = discord.Intents.all() # 啟用所有 intents
 activity = discord.Game('[C] | Chelp 以資查詢')
-bot = commands.Bot(command_prefix=Prefix, owner_ids=Owner_id, intents = intents)
+bot = commands.Bot(
+    command_prefix = Prefix, 
+    owner_ids = Owner_id, 
+    intents = intents
+)
 bot.remove_command("help")
-
+status = cycle([
+    "[C] 開發者 CC_#8844", 
+    "[C] | Chelp 以資查詢", 
+    "指令前綴  `C`"
+])
 
 @bot.event
 async def on_ready():
   await bot.change_presence(activity=discord.Game(activity))
   print(f'{bot.user.name}已成功上線！')
     
-
+@tasks.loop(seconds=10)
+async def status_swap():
+  await bot.change_presence(
+      activity = discord.Activity(
+          type = discord.ActivityType.watching,
+          name = (next(status))
+      )
+  )
 
 for filename in os.listdir('./Commands'):
     if filename.endswith('.py'):
