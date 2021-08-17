18e5dbd92508c21a2c6925bcf9d7f24ed4bbf537
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2021-08-17 19:58:54 +0800

    Update time.py

--- Commands/time.py
+++ Commands/time.py
@@ -13,10 +13,16 @@ class Time(commands.Cog):
       while not self.bot.is_closed():
         now2 = datetime.now(tz).strftime("📅次年●%m月%d日●")
         now = datetime.now(tz).strftime("🕠現在時刻： %H 點")
+        guild = self.bot.get_guild(869781588483924069)
+        member_count = len(ctx.guild.members) # includes bots
+        true_member_count = len([m for m in ctx.guild.members if not m.bot])
+        
         channel = self.bot.get_channel(852346393141182484) # ID des Channels
         channel2 = self.bot.get_channel(852364573095755808)
         channel3 = self.bot.get_channel(877156561695412254)
         channel4 = self.bot.get_channel(877156925341597736)
+        channel5 = self.bot.get_channel(877159224856170508) # ID des Channels
+
         time = datetime.now(tz).strftime('[%Y-%m-%d] [%H:%M]')
         print(f'已於：{time} 更換頻道時間。')
         await channel.edit(name=now) # Füge hier den ersten Channel Namen ein zu dem gewechselt werden soll.
@@ -25,7 +31,9 @@ class Time(commands.Cog):
         await asyncio.sleep(1)
         await channel3.edit(name=now2)
         await asyncio.sleep(1)
-        await channel4.edit(name=now)# Füge hier den ersten Channel Namen ein zu dem gewechselt werden soll.        
+        await channel4.edit(name=now)# Füge hier den ersten Channel Namen ein zu dem gewechselt werden soll.      
+        await asyncio.sleep(1)
+        await channel5.edit(name='馬卡島成員數：' + true_member_count)# Füge hier den ersten Channel Namen ein zu dem gewechselt werden soll.      
         await asyncio.sleep(310) # Hier die sleep dauer // Nicht unter 300 [Discord - RateLimit]
     self.bg_task = self.bot.loop.create_task(interval())
 
