b984cf64dde5090ef13e7efb70543f1bcc0b348e
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2021-08-17 19:48:50 +0800

    Update time.py

--- Commands/time.py
+++ Commands/time.py
@@ -15,11 +15,15 @@ class Time(commands.Cog):
         now = datetime.now(tz).strftime("🕠現在時刻： %H 點")
         channel = self.bot.get_channel(852346393141182484) # ID des Channels
         channel2 = self.bot.get_channel(852364573095755808)
+        channel3 = self.bot.get_channel(877156561695412254)
+        channel4 = self.bot.get_channel(877156925341597736)
         time = datetime.now(tz).strftime('[%Y-%m-%d] [%H:%M]')
         print(f'已於：{time} 更換頻道時間。')
         await channel.edit(name=now) # Füge hier den ersten Channel Namen ein zu dem gewechselt werden soll.
         await asyncio.sleep(1)
-        await channel2.edit(name=now2) # Füge hier den ersten Channel Namen ein zu dem gewechselt werden soll.        
+        await channel2.edit(name=now2)
+        await channel3.edit(name=now2)
+        await channel4.edit(name=now)# Füge hier den ersten Channel Namen ein zu dem gewechselt werden soll.        
         await asyncio.sleep(310) # Hier die sleep dauer // Nicht unter 300 [Discord - RateLimit]
     self.bg_task = self.bot.loop.create_task(interval())
 
